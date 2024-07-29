b029f88f42f991880b7b6a3495d0247e41d467b9
Carles <geerles@gmail.com>
2024-07-29 12:11:37 +0200

    add env

--- src/api/github.api.ts
+++ src/api/github.api.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
+    headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    }
 });
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -6,25 +6,6 @@ export const useLabels = () => {
         queryKey: ['labels'],
         queryFn: getLabels,
         staleTime: 1000 * 60 * 60, // 1 hora de cache,
-        placeholderData: [
-          {
-            "id": 6955781886,
-            "node_id": "LA_kwDOAJy2Ks8AAAABnpjO_g",
-            "url": "https://api.github.com/repos/facebook/react/labels/Compiler:%20todo",
-            "name": "Compiler: todo",
-            "color": "C2E0C6",
-            "default": false
-          },
-          {
-            "id": 1205087127,
-            "node_id": "MDU6TGFiZWwxMjA1MDg3MTI3",
-            "url": "https://api.github.com/repos/facebook/react/labels/Component:%20Concurrent%20Features",
-            "name": "Component: Concurrent Features",
-            "color": "ffccd3",
-            "default": false,
-            "description": ""
-          },
-        ]
       });
   return {labelsQuery}
 }
